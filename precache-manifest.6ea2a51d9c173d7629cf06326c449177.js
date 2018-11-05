84f0486c81082f08b524decc09f428535c43cf92
mike <mike.b.mcintosh@gmail.com>
2018-11-05 15:22:57 -0500

    Updates

---
+++ precache-manifest.6ea2a51d9c173d7629cf06326c449177.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce9864f4a466cbf49f34",
+    "url": "/test-slider-app/static/css/main.ce365c43.chunk.css"
+  },
+  {
+    "revision": "ce9864f4a466cbf49f34",
+    "url": "/test-slider-app/static/js/main.ce9864f4.chunk.js"
+  },
+  {
+    "revision": "169a7c49eb29c1778014",
+    "url": "/test-slider-app/static/js/1.169a7c49.chunk.js"
+  },
+  {
+    "revision": "c9e1d8572fcdfdb15ae7",
+    "url": "/test-slider-app/static/js/runtime~main.c9e1d857.js"
+  },
+  {
+    "revision": "b45138c9bdebae13dcf2fc40092516b8",
+    "url": "/test-slider-app/index.html"
+  }
+];
